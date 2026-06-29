8637bf65cbdabbef4ae0bd0ad68bd5793bca78db
AMID GWAPO <jrtisoy96@outlook.com>
2026-06-29 23:04:21 +0800

    updates

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using AmidPortfolio;
 using AmidPortfolio.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -15,4 +16,18 @@ builder.Services.AddSingleton<ThemeService>();
 builder.Services.AddScoped<NavigationService>();
 builder.Services.AddScoped<NavigationState>();
 builder.Services.AddSingleton<GradientService>();
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.RequireInteraction = false;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
 await builder.Build().RunAsync();
